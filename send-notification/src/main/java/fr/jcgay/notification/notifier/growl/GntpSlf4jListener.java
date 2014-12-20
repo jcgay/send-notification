56b5322361488acbd9a057bdd4dd47530a83188c
Jean-Christophe GAY <contact@jeanchristophegay.com>
2014-12-20 15:31:31 +0100

    Complete Growl error messages with wiki links
    These links add informations and possible solution(s) for common misconfiguration problems.

--- send-notification/src/main/java/fr/jcgay/notification/notifier/growl/GntpSlf4jListener.java
+++ send-notification/src/main/java/fr/jcgay/notification/notifier/growl/GntpSlf4jListener.java
@@ -10,6 +10,9 @@ class GntpSlf4jListener implements GntpListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GntpSlf4jListener.class);
 
+    private static final String OSX_URL = "https://github.com/jcgay/send-notification/wiki/Growl-(OS-X)";
+    private static final String WINDOWS_URL = "https://github.com/jcgay/send-notification/wiki/Growl-(Windows)";
+
     @Override
     public void onRegistrationSuccess() {
         LOGGER.debug("Client has been successfully registered by Growl");
@@ -37,16 +40,32 @@ class GntpSlf4jListener implements GntpListener {
 
     @Override
     public void onRegistrationError(GntpErrorStatus status, String description) {
-        LOGGER.error("Growl registration has failed.\n {}: {}", status, description);
+        LOGGER.error(error("Growl registration has failed.\n {}: {}"), status, description);
     }
 
     @Override
     public void onNotificationError(GntpNotification notification, GntpErrorStatus status, String description) {
-        LOGGER.error("Growl notification has failed.\n {}: {}\n {}", status, description, notification);
+        LOGGER.error(error("Growl notification has failed.\n {}: {}\n {}"), status, description, notification);
     }
 
     @Override
     public void onCommunicationError(Throwable t) {
         LOGGER.error("Cannot communicate with Growl.", t);
+    }
+
+    private static String error(String message) {
+        return String.format("%s%n%n For more information about the errors and possible solutions, please read the following article:%n%s",
+                message, wikiUrl());
+    }
+
+    private static String wikiUrl() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")) {
+            return WINDOWS_URL;
+        }
+        if (os.contains("mac")) {
+            return OSX_URL;
+        }
+        return "https://github.com/jcgay/send-notification/wiki";
     }
 }
